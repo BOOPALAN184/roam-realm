a7ac40d269db7278b62c605f2c6e7656919246ad
BOOPALAN A <164721446+BOOPALAN184@users.noreply.github.com>
2025-03-26 20:13:53 +0530

    Add files via upload

---
+++ WEB/main.js
@@ -0,0 +1,11 @@
+// discover container
+ScrollReveal().reveal(".discover__card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+ScrollReveal().reveal(".discover__card__content", {
+    ...scrollRevealOption,
+    interval: 500,
+    delay: 200,
+});
